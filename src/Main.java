5d624f194cf20f2c3209e1524a516b830ac5486b
melissarsousa <melissa.rios.sousa@gmail.com>
2022-12-14 11:04:07 -0300

    Criação de classes e packages

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Java");
+        curso1.setDescricao("aprendendo sobre Java");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("aprendendo Java na prática");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
+
+    }
+}
